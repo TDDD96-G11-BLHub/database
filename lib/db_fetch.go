d5009ffe76dbfadce5c78ff576b5eae31897fde6
MattKa01 <mattias@tailspin.se>
2024-04-04 11:55:51 +0200

    Removed uri from code :pig2:

--- lib/db_fetch.go
+++ lib/db_fetch.go
@@ -1,7 +1,35 @@
 package lib
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 func FetchHello() {
 	fmt.Println("Hello, this is the fetch package")
+}
+
+func FetchCollection(client mongo.Client, name string) []byte {
+
+	coll := client.Database("Sensordata").Collection(name)
+	time := "15:40:22"
+	var result bson.M
+	err := coll.FindOne(context.TODO(), bson.D{{"Time", time}}).Decode(&result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the time %s\n", time)
+	}
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+
+	return jsonData
 }
