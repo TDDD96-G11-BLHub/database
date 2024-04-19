e5688da2b683e526ca06c4418fb8aabcde36e7a3
MattKa01 <mattias@tailspin.se>
2024-04-19 13:24:12 +0200

    Started Implementing peer to peer database from scratch

---
+++ peer/baseinterface.go
@@ -0,0 +1,82 @@
+package peer
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo/address"
+)
+
+//TODO: Create different types of LocalDB struct. (Document, user, sensor, more...)
+//TODO: Use correct type of context in functions, not just TODO
+//TODO: Create and use slog in functions
+
+type LocalDBBase interface {
+	ID() *Identity
+
+	Open(ctx context.Context, collAdress string) (Collection, error)
+
+	Create(ctx context.Context, name string) (Collection, error)
+
+	GetAddress(ctx context.Context, name string) (address.Address, error)
+
+	Logger() *slog.Logger
+}
+
+type Identity struct {
+	iD         []byte
+	pubKey     []byte
+	signatures []*IDSignatures
+}
+
+type IDSignatures struct {
+	iD     []byte
+	pubKey []byte
+}
+
+type LocalDB struct {
+	name        string
+	path        address.Address
+	collections []*Collection
+	log         *slog.Logger
+}
+
+type Collection struct {
+	name string
+	file *os.File
+	keys []byte
+	log  *slog.Logger
+}
+
+// type CreateCollOptions struct {
+// 	Directory   string
+// 	LocalOnly   bool
+// 	Overwrite   bool
+// 	ContentType string
+// 	Logger      *slog.Logger
+// }
+
+func (db *LocalDB) Create(ctx context.Context, name string) (*Collection, error) {
+	file, err := os.Create(db.path.String() + "/" + name + ".json")
+	if err != nil {
+		db.log.Error("Could not create file")
+	}
+	coll := Collection{name, file, []byte(""), db.log}
+	db.collections = append(db.collections, &coll)
+	return &coll, err
+}
+
+func CreateLocalDatabase(ctx context.Context, name string, path address.Address, log slog.Logger) (*LocalDB, error) {
+	err := os.Mkdir(path.String()+"/"+name, 0777)
+	if err != nil {
+		log.Error("Could not create directory")
+	}
+	var collections []*Collection
+	db := LocalDB{name, path, collections, &log}
+	return &db, err
+}
+
+func (db *LocalDB) Logger() *slog.Logger {
+	return db.log
+}
