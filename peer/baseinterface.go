ca5c25772e4ced20bf3c8af26b84ca5330de5d5f
Mattias Karlsson <mattias@tailspin.se>
2024-04-20 08:58:14 +0200

    stuff

--- peer/baseinterface.go
+++ peer/baseinterface.go
@@ -11,13 +11,20 @@ import (
 //TODO: Create different types of LocalDB struct. (Document, user, sensor, more...)
 //TODO: Use correct type of context in functions, not just TODO
 //TODO: Create and use slog in functions
+//TODO: More descriptive errors, maybe even just return them
+//TODO: Maybe just use string instead of address package
+//TODO: Check if it is safe to save open file pointers in struct while they are open
+//TODO: Add Defer functions to close files on crash
+//TODO: Add keys to collections in write function
 
 type LocalDBBase interface {
 	ID() *Identity
 
-	Open(ctx context.Context, collAdress string) (Collection, error)
+	Open(ctx context.Context, collection *Collection) (*os.File, error)
 
 	Create(ctx context.Context, name string) (Collection, error)
+
+	Write(ctx context.Context, collection *Collection, content []byte) (int, error)
 
 	GetAddress(ctx context.Context, name string) (address.Address, error)
 
@@ -62,7 +69,8 @@ func (db *LocalDB) Create(ctx context.Context, name string) (*Collection, error)
 	if err != nil {
 		db.log.Error("Could not create file")
 	}
-	coll := Collection{name, file, []byte(""), db.log}
+	file.Close()
+	coll := Collection{name, nil, []byte(""), db.log}
 	db.collections = append(db.collections, &coll)
 	return &coll, err
 }
@@ -79,4 +87,24 @@ func CreateLocalDatabase(ctx context.Context, name string, path address.Address,
 
 func (db *LocalDB) Logger() *slog.Logger {
 	return db.log
+}
+
+func (db *LocalDB) Open(ctx context.Context, collection *Collection) (*os.File, error) {
+	file, err := os.Open(db.path.String() + "/" + collection.name)
+	if err != nil {
+		db.log.Error("Could not open file")
+	}
+	collection.file = file
+	return file, err
+}
+
+func (db *LocalDB) Write(ctx context.Context, collection *Collection, content []byte) (int, error) {
+	if collection.file == nil {
+		db.Open(ctx, collection)
+	}
+	bytes, err := collection.file.Write(content)
+	if err != nil {
+		db.log.Error("Could not write to file")
+	}
+	return bytes, err
 }
